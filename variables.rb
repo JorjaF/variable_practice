9dc38954a9a85bdae4a8cce8421f43da78c16c8b
Jorja Fleming <aset284@gmail.com>
2023-02-22 17:12:54 -0700

    Add puts and correct spelling

--- variables.rb
+++ variables.rb
@@ -1,11 +1,19 @@
 pet_1 = "Spatz"
 pet_2 = "Hammi"
 
+puts "pet_1 is #{pet_1}"
+
 age = 43
 phone = 7204359199
 
 height = 5.2
 balance = 123.45
 
-has_paid = yes
+has_paid = true
 original_nose = false
+
+pet_1 = "Mookie"
+pet_2 = "Leopold"
+
+puts "pet_1 is #{pet_1}"
+
